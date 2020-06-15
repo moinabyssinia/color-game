4831b7977cd751b382a7e84ca9c42a7491405f3e
moinabyssinia <michaelg.tadesse@gmail.com>
2020-06-15 12:20:01 -0400

    First commit

---
+++ colorGame.js
@@ -0,0 +1,43 @@
+//fixed colors
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+]
+
+// //randomized colors
+// var x = Math.ceil(Math.random()*255);
+// var y = Math.ceil(Math.random()*255);
+// var z = Math.ceil(Math.random()*255);
+
+var squares = document.querySelectorAll(".square");
+
+//pick a color to display at the top
+var pickedColor = colors[3];
+
+//display picked color
+var heading = document.getElementsByClassName("color-display")
+heading[0].textContent = pickedColor.toUpperCase();
+
+for(var i = 0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+}
+
+//check if user selected correct color
+
+//add event listener to squares
+for(var i = 0; i < squares.length; i++){
+    squares[i].addEventListener("click", function(){
+        if(this.style.backgroundColor === pickedColor.toLowerCase()){
+            // iterate over boxes to change color
+            for(var j = 0; j < squares.length; j++){
+                squares[j].style.backgroundColor = pickedColor;
+            };
+        } else{
+            alert("wrong, try again")
+        }
+    });
+}
